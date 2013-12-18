5971028826689f20a004a75929df575b841367dc
Jeremiah Sebastian Samatra <jeremiah_cool01@yahoo.com>
2013-12-18 10:32:05 +0800

    Upload files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Uploading::Application.config.secret_key_base = '4e0641c981de45a84996a1930ad6f26917dc666e612911a7169be4d9fb73df7419a48168be50adbf0ca3283fdf3ad79436078d7634906ccffa0c08fe80e6772d'
