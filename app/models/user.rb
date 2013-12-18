f1fbd38bf49f7d587f178c00b97b71b1d4f48311
Jeremiah Sebastian Samatra <jeremiah_cool01@yahoo.com>
2013-12-18 11:25:58 +0800

    pagination with design

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
 validates :name, :presence => true
 validates :email, :presence => true
-has_attached_file :pic, :styles => { :medium => "300x300>", :thumb => "300x300>" }
+has_attached_file :pic, :styles => { :medium => "300x300>", :thumb => "300x300>", :small => "400x400" }
 has_attached_file :attach
+self.per_page = 2
 end
+WillPaginate.per_page = 2
